3d9a845157eb78bc2d60bd33fbba0f7dc878c5a7
Zale37 <alexandre.t.costa@ubi.pt>
2025-06-18 10:28:09 +0100

    update_visual

---
+++ frontend/2BESEEN/src/js/index.js
@@ -0,0 +1,24 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) { showSlides(slideIndex += n); }
+function currentSlide(n) { showSlides(slideIndex = n); }
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slide");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].classList.add("active");
+    dots[slideIndex-1].className += " active";
+}
+
+// Auto-slide (opcional)
+setInterval(() => { plusSlides(1); }, 6000);
